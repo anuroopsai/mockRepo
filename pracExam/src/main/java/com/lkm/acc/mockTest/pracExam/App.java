a47082ac19dd3bf74a2d451143dd0416fb3a6b79
anuroopsai <anuroopsai@gmail.com>
2023-12-19 17:03:22 +0530

    initial Commit

---
+++ pracExam/src/main/java/com/lkm/acc/mockTest/pracExam/App.java
@@ -0,0 +1,13 @@
+package com.lkm.acc.mockTest.pracExam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pracExam/src/main/java/com/lkm/acc/mockTest/pracExam/TreeMapDem.java
@@ -0,0 +1,15 @@
+package com.lkm.acc.mockTest.pracExam;
+import java.util.*;
+public class TreeMapDem {
+	public static void main(String args[]) {
+		
+	Map<String,Integer> tm = new TreeMap<String,Integer>();
+	tm.put("siva",100);
+	tm.put("krishna",90);
+	tm.put("ram",88);
+	tm.put("vishnu",80);
+	
+	
+	System.out.println(tm);
+	}
+}
